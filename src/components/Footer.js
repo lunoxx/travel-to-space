85ac44fe9328948cb0559403e04d7133342f6cf5
lunoxx <a.mese99@gmail.com>
2023-02-27 00:20:03 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <div className='website-footer right-nav flex-col text-gray-100 w-full text-center capitalize h-14 flex justify-center items-center'>
         Alin Meseșan &copy; 2022
-        <a className = "text-gray-400" href = "https://www.frontendmentor.io/challenges/space-tourism-multipage-website-gRWj1URZ3" target="_blank">Frontend Mentor Challenge</a>
+        <a className = "text-gray-400" href = "https://www.frontendmentor.io/challenges/space-tourism-multipage-website-gRWj1URZ3" target="_blank" rel="noreferrer">Frontend Mentor Challenge</a>
     </div>
   )
 }
