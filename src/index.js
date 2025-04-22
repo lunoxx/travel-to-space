9da059819926feddda52e6a4d11f1847a8950063
lunoxx <a.mese99@gmail.com>
2025-04-22 16:53:56 +0300

    build

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter  } from 'react-router-dom';
 
 import App from './App';
 
@@ -15,12 +15,10 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
-     <BrowserRouter>
-        <App />
-     </BrowserRouter>
-    <Footer/>
-
+    <HashRouter >
+      <App />
+      <Footer />
+    </HashRouter >
   </React.StrictMode>
 );
 
